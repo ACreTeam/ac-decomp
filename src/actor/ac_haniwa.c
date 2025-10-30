cf7599ef2cb78976bc9b84b4d73f1c29e1ee6c73
Cuyler36 <Cuyler36@users.noreply.github.com>
2025-10-29 23:09:03 -0400

    ac_haniwa: refactor

--- src/actor/ac_haniwa.c
+++ src/actor/ac_haniwa.c
@@ -31,36 +31,40 @@ static void aHNW_actor_dt(ACTOR* actor, GAME* game);
 static void aHNW_actor_init(ACTOR* actor, GAME* game);
 static void aHNW_actor_draw(ACTOR* actor, GAME* game);
 
+// clang-format off
 ACTOR_PROFILE Haniwa_Profile = {
-    mAc_PROFILE_HANIWA,   ACTOR_PART_BG,  ACTOR_STATE_NONE, ACTOR_PROP_HANIWA0,       ACTOR_OBJ_BANK_12,
+    mAc_PROFILE_HANIWA,
+    ACTOR_PART_BG,
+    ACTOR_STATE_NONE,
+    ACTOR_PROP_HANIWA0,
+    ACTOR_OBJ_BANK_12,
     sizeof(HANIWA_ACTOR),
-
-    &aHNW_actor_ct,       &aHNW_actor_dt, &aHNW_actor_init, (mActor_proc)&none_proc1, NULL
+    &aHNW_actor_ct,
+    &aHNW_actor_dt,
+    &aHNW_actor_init,
+    mActor_NONE_PROC1,
+    NULL,
 };
+// clang-format on
 
 static ClObjPipeData_c AcHaniwaCoInfoData = { { 57, 32, ClObj_TYPE_PIPE }, { 1 }, { 20, 30, 0, { 0, 0, 0 } } };
 
-static StatusData_c AcHaniwaStatusData = { 0, 20, 30, 0, 254 };
+static StatusData_c AcHaniwaStatusData = { 0, 20, 30, 0, MASSTYPE_HEAVY };
 
 /* TODO: ct, dt, & draw are in their own TU */
 static void aHNW_actor_ct(ACTOR* actor, GAME* game) {
     HANIWA_ACTOR* haniwa = (HANIWA_ACTOR*)actor;
-    ClObjPipe_c* pipe;
     cKF_SkeletonInfo_R_c* keyframe = &haniwa->common_actor_class.anime.keyframe;
-    GAME_PLAY* play = (GAME_PLAY*)game;
 
     cKF_SkeletonInfo_R_ct(keyframe, &cKF_bs_r_hnw, NULL, haniwa->keyframe_work_area, haniwa->keyframe_morph_area);
-
-    pipe = &haniwa->common_actor_class.col_pipe;
-    ClObjPipe_ct(game, pipe);
-    ClObjPipe_set5(game, pipe, actor, &AcHaniwaCoInfoData);
-    CollisionCheck_Status_set3(&haniwa->common_actor_class.actor_class.status_data, &AcHaniwaStatusData);
-
     {
-        Object_Bank_c* bank = &play->object_exchange.banks[actor->data_bank_id];
-        haniwa->bank_ram_start = bank->ram_start;
+        ClObjPipe_c* pipe = &haniwa->common_actor_class.col_pipe;
+        ClObjPipe_ct(game, pipe);
+        ClObjPipe_set5(game, pipe, actor, &AcHaniwaCoInfoData);
+        CollisionCheck_Status_set3(&haniwa->common_actor_class.actor_class.status_data, &AcHaniwaStatusData);
     }
 
+    haniwa->bank_ram_start = ((GAME_PLAY*)game)->object_exchange.banks[actor->data_bank_id].ram_start;
     haniwa->common_actor_class.anime.anime_no = 2;
     haniwa->house_idx = actor->npc_id - ACTOR_PROP_HANIWA0;
     actor->talk_distance = 43.0f;
@@ -68,10 +72,9 @@ static void aHNW_actor_ct(ACTOR* actor, GAME* game) {
 
 static void aHNW_actor_dt(ACTOR* actor, GAME* game) {
     HANIWA_ACTOR* haniwa = (HANIWA_ACTOR*)actor;
-    GAME_PLAY* play = (GAME_PLAY*)game;
 
     if (haniwa->playing_save_bgm) {
-        mBGMPsComp_delete_ps_demo(0x41, 0x168);
+        mBGMPsComp_delete_ps_demo(BGM_ENTER_HOUSE, 0x168);
     }
 
     cKF_SkeletonInfo_R_dt(&haniwa->common_actor_class.anime.keyframe);
@@ -89,31 +92,27 @@ static void aHNW_actor_draw(ACTOR* actor, GAME* game) {
     HANIWA_ACTOR* haniwa = (HANIWA_ACTOR*)actor;
     cKF_SkeletonInfo_R_c* keyframe = &haniwa->common_actor_class.anime.keyframe;
     GRAPH* g = game->graph;
-    Mtx* m;
-
-    m = GRAPH_ALLOC_TYPE(g, Mtx, keyframe->skeleton->num_shown_joints);
+    Mtx* m = GRAPH_ALLOC_TYPE(g, Mtx, keyframe->skeleton->num_shown_joints);
 
     if (m != NULL) {
-        Gfx* gfx;
         int house_idx = haniwa->house_idx;
+
         _texture_z_light_fog_prim(g);
 
-        OPEN_DISP(g);
-        gfx = NOW_POLY_OPA_DISP;
+        OPEN_POLY_OPA_DISP(g);
 
-        gSPSegment(gfx++, G_MWO_SEGMENT_B, hnw_tmem_txt);
+        gSPSegment(POLY_OPA_DISP++, ANIME_4_TXT_SEG, hnw_tmem_txt);
 
         if (mPr_NullCheckPersonalID(&Save_Get(homes[house_idx]).ownerID) != TRUE &&
             Common_Get(player_no) == mHS_get_pl_no(house_idx)) {
-            gDPSetPrimColor(gfx++, 0, 128, 255, 255, 255, 255);
+            gDPSetPrimColor(POLY_OPA_DISP++, 0, 128, 255, 255, 255, 255);
         } else {
-            gDPSetPrimColor(gfx++, 0, 128, 255, 255, 255, 255);
+            gDPSetPrimColor(POLY_OPA_DISP++, 0, 128, 255, 255, 255, 255);
         }
 
-        gSPDisplayList(gfx++, hnw_tex_model);
+        gSPDisplayList(POLY_OPA_DISP++, hnw_tex_model);
 
-        SET_POLY_OPA_DISP(gfx);
-        CLOSE_DISP(g);
+        CLOSE_POLY_OPA_DISP(g);
 
         cKF_Si3_draw_R_SV(game, keyframe, m, NULL, NULL, actor);
     }
